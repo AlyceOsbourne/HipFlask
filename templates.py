5549f2fb5927616693e2024ab8542cdd4f262428
Alyce <Alyceosbourne@outlook.com>
2022-12-25 16:04:22 +0000

    Made a small mixin system to make building templates easier. Uses a search or create system to make sure that the core structure exists

--- templates.py
+++ templates.py
@@ -1,107 +1,92 @@
-from bs4 import BeautifulSoup
-
-from flask_orm import JinjaBlock, JinjaComment
-from tags import *
-
-
-def nav_bar(*links, dark = False):
-    nav = Nav()
-    ul = Ul()
-    for link in links:
-        ul >> [Li() >> [A(href = link[1]) >> link[0]]]  # noqa
-    return nav >> ul
+from bootstrap_tags import *
+from flask_orm import (
+    JinjaBlock,
+    Tag,
+)
 
 
-def default_head_structure():
-    title = Title()
-    meta = Meta(
-            charset = "utf-8",
-            name = "viewport",
-            content = "width=device-width, initial-scale=1.0",
-    )
-    return title, meta
+class PageTemplate:
+    html: str | Tag = Html()
+    head: str | Tag = Head()
+    title: str | Tag = Title()
+    meta: str | Tag = Meta() >> {
+            "charset": "utf-8",
+            "name"   : "viewport",
+            "content": "width=device-width, initial-scale=1, shrink-to-fit=no",
+    }
+    body: str | Tag = Body()
+    root: str | Tag = Root() >> [html >> [head >> [title, meta], body]]
 
-
-def default_file_structure():
-    root = Root()
-    html = Html()
-    head = Head()
-    body = Body()
-    title, meta = default_head_structure()
-
-    return (
-            root >> [html >> [head >> [title, meta], body]],
-            html,
-            head,
-            title,
-            meta,
-            body,
-    )
-
-
-def bootstrap():
-    bootstrap_css = StyleSheet(
-            href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
-            integrity = "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T",
-            crossorigin = "anonymous",
-    )
-    bootstrap_js = Script(
-            src = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-            integrity = "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM",
-            crossorigin = "anonymous",
-    )
-    jquery = Script(
-            src = "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-            integrity = "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
-            crossorigin = "anonymous",
-    )
-    popper = Script(
-            src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-            integrity = "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1",
-            crossorigin = "anonymous",
-    )
-    return bootstrap_css, bootstrap_js, jquery, popper
-
-
-class Page:
-    root, html, head, title, meta, body = default_file_structure()
-
-    def __init__(
-            self, /, *, root = None, html = None, head = None, body = None, title = None, meta = None
-    ):
-        self.root = root
+    def __init__(self, /, *,
+                 title = None,
+                 html = None,
+                 head = None,
+                 body = None):
+        self.title = title
         self.html = html
         self.head = head
         self.body = body
-        self.title = title
-        self.meta = meta
 
     def __str__(self):
         return str(self.root)
 
 
+class BootstrapMixin:
+    bootstrap_css: str | Tag = BootstrapStyleSheet()
+    bootstrap_js: str | Tag = BootStrapScript()
+    jquery: str | Tag = BootstrapJQuery()
+
+    def __init_subclass__(cls, /, *, bootstrap = True, **kwargs):
+        # make sure mixin is being applied to a subclass of PageTemplate
+        if not issubclass(cls, PageTemplate):
+            raise TypeError("BootstrapMixin must be applied to a subclass of PageTemplate")
+        super().__init_subclass__(**kwargs)
+        if bootstrap:
+            if not hasattr(cls, "head"):
+                cls.head = Head()
+            getattr(cls, "head") >> [getattr(cls, "bootstrap_css")]
+            if not hasattr(cls, "body"):
+                cls.body = Body()
+            getattr(cls, "body") >> [getattr(cls, "bootstrap_js"), getattr(cls, "jquery")]
+            if not hasattr(cls, "root"):
+                cls.root = Root()
+            getattr(cls, "root") >> [getattr(cls, "html") >> [getattr(cls, "head"), getattr(cls, "body")]]
+        return cls
+
+
+class JinjaMixin:
+    title_block: str | Tag = JinjaBlock("title")
+    head_block: str | Tag = JinjaBlock("head")
+    css_block: str | Tag = JinjaBlock("css")
+    body_block: str | Tag = JinjaBlock("body")
+    js_block: str | Tag = JinjaBlock("js")
+
+    def __init_subclass__(cls, /, *, jinja = True, **kwargs):
+        # make sure mixin is being applied to a subclass of PageTemplate
+        if not issubclass(cls, PageTemplate):
+            raise TypeError("JinjaMixin must be applied to a subclass of PageTemplate")
+        super().__init_subclass__(**kwargs)
+        if jinja:
+            if not hasattr(cls, "title"):
+                cls.title = Title()
+            getattr(cls, "title") >> getattr(cls, "title_block")
+            if not hasattr(cls, "head"):
+                cls.head = Head()
+            getattr(cls, "head") >> getattr(cls, "head_block")
+            if not hasattr(cls, "body"):
+                cls.body = Body()
+            getattr(cls, "body") >> getattr(cls, "body_block")
+            if not hasattr(cls, "root"):
+                cls.root = Root()
+            getattr(cls, "root") >> [getattr(cls, "html") >> [getattr(cls, "head"), getattr(cls, "body")]]
+        return cls
+
+
+class Page(PageTemplate, BootstrapMixin, JinjaMixin):
+    def __init__(self, /, **kwargs):
+        super().__init__(**kwargs)
 
 
 if __name__ == "__main__":
-    page = Page(
-            title = JinjaBlock("title") >> "Hello World",
-            head = [
-                    JinjaComment("Loading Bootstrap"),
-                    bootstrap(),
-                    JinjaBlock("head"),
-                    JinjaBlock("css"),
-                    JinjaBlock("js"),
-            ],
-            body = [
-                    nav_bar(("Home", "/"), ("About", "/about"), ("Contact", "/contact")),
-                    JinjaBlock("content"),
-                    Footer() >> "© 2022",
-            ],
-    )
-
-    soup = BeautifulSoup(str(page), "html.parser")
-    pretty = soup.prettify()
-    print(pretty)
-
-    with open("index.html", "w") as f:
-        f.write(pretty)
+    page = Page(title = "Hello World")
+    print(page)
