b7610a56aa56acc42ba133002d52d82a65a3031d
Alyce <Alyceosbourne@outlook.com>
2022-12-25 00:23:16 +0000

    Experimenting to see what kind of templating works best

--- tests.py
+++ tests.py
@@ -1,104 +1,60 @@
-from flask import Blueprint, render_template_string
+from tags import *
 
-from flask_orm import JinjaBlock
-from tags import Body, Footer, Head, Html, Meta, NavBar, pretty_print, Root, ScriptFile, StyleSheet, Title
 
-generated = Blueprint("generated", __name__, template_folder = "templates")
+def head_template(title, stylesheets = None, scripts = None):
+    head = Head()
+    head_block = JinjaBlock("head")
+    title = Title() >> title
+    meta = Meta() >> {
+            "charset": "utf-8",
+            "name"   : "viewport",
+            "content": "width=device-width, initial-scale=1, shrink-to-fit=no",
+    }
+    if stylesheets is None:
+        stylesheets = []
+    if scripts is None:
+        scripts = []
+    return head >> head_block >> title >> meta >> stylesheets >> scripts
+
+
+def body_template(nav_bar,  footer):
+    body = Body()
+    body_block = JinjaBlock("body")
+    return body >> nav_bar >> body_block >> footer
+
+
+def nav_bar_template(*pages):
+    nav_bar = NavBar()
+    ul = Ul()
+    for page in pages:
+        ul >> (Li >> (A >> {"href": page["href"]} >> page["name"]))
+    return nav_bar >> ul
+
+
+def footer_template():
+    footer = Footer()
+    footer_block = JinjaBlock("footer")
+    return footer >> footer_block
 
 
 class Page:
     _root = Root()
     _html = Html()
+    _head = head_template("Page Title")
+    _body = body_template(
+            nav_bar_template(
+                    {"name": "Home", "href": "/"},
+                    {"name": "About", "href": "/about"},
+                    {"name": "Contact", "href": "/contact"},
+            ),
+            footer_template(),
+    )
 
-    _head = Head()
-
-    _title = Title()
-    _title_block = JinjaBlock("title")
-
-    _css = JinjaBlock("css")
-    _meta = Meta() >> {
-            "charset"   : "utf-8",
-            "name"      : "viewport",
-            "content"   : "width=device-width, initial-scale=1, shrink-to-fit=no",
-            "http-equiv": "X-UA-Compatible",
-    }
-
-    _body = Body()
-    _body_block = JinjaBlock("body")
-    _js = JinjaBlock("js")
-
-    _root >> (_html >> (_head >> (_title >> _title_block) >> _css >> _meta) >> (_body >> _body_block >> _js))
-
-    def __init_subclass__(cls, route = None, **kwargs):
-        super().__init_subclass__(**kwargs)
-        if route is None:
-            return
-
-        @generated.route(f"/{route}")
-        def view():
-            return render_template_string(str(cls._root))
-
-        cls._route = route
+    _root >> _html >> _head >> _body
 
     def __str__(self):
-        return str(self._root)
-
-
-
-class Template(Page):
-    def __init__(
-        self,
-        title: str,
-    ):
-        self._title_block = title
-        self._body_block = (
-            NavBar >> JinjaBlock("navbar"),
-            JinjaBlock("content"),
-            Footer >> JinjaBlock("footer"),
-        )
-
-        self._css = (
-            StyleSheet
-            >> {
-                "rel": "stylesheet",
-                "href": "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
-                "integrity": "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T",
-                "crossorigin": "anonymous",
-            },
-            StyleSheet
-            >> {
-                "rel": "stylesheet",
-                "href": "https://use.fontawesome.com/releases/v5.8.2/css/all.css",
-                "integrity": "sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay",
-                "crossorigin": "anonymous",
-            },
-        )
-
-        self._js = (
-            ScriptFile
-            >> {
-                "src": "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-                "integrity": "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
-                "crossorigin": "anonymous",
-            },
-            ScriptFile
-            >> {
-                "src": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
-                "integrity": "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1",
-                "crossorigin": "anonymous",
-            },
-            ScriptFile
-            >> {
-                "src": "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
-                "integrity": "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM",
-                "crossorigin": "anonymous",
-            },
-        )
+        return self._root
 
 
 if __name__ == "__main__":
-    pretty_print(
-        Template(
-            title="Test",
-        )
-    )
+    print(pretty_print(Page()))
